9a8a18f4c90eea1834c35d22447fe3fcd567069f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-05 09:40:14 +0600

    _mod04_person_student_lt

---
+++ module04/person.php
@@ -0,0 +1,21 @@
+<?php
+class Person {
+    
+    public $name;
+    public $age;
+
+ 
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+        public function introduce() {
+        echo "My name is {$this->name} and I am {$this->age} years old.";
+    }
+}
+
+$person = new Person("John", 30);
+$person->introduce();
+
+?>
